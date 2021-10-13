cf5eba0e7959d64f87ed532d4a28028407531b55
quentinDyma <quentindevial19@gmail.com>
2021-10-13 15:54:38 +0200

    fix tls

--- setup-tls.sh
+++ setup-tls.sh
@@ -1,5 +1,6 @@
-curl https://get.acme.sh | sh -s email=contact@dyma.fr
-sudo apt install socat
-sh ~/.acme.sh/acme.sh --issue -d dymawonder.fr -d www.dymawonder.fr --standalone \
---pre-hook "docker-compose -f /home/ubuntu/docker-chapitre12-environnement-symfony/docker-compose.prod.yml stop" \
---post-hook "docker-compose -f /home/ubuntu/docker-chapitre12-environnement-symfony/docker-compose.prod.yml start"
+# sudo su
+# curl https://get.acme.sh | sh -s email=contact@dyma.fr
+# apt install socat
+# sh /root/.acme.sh/acme.sh --issue -d dymawonder.fr -d www.dymawonder.fr --standalone \
+# --pre-hook "docker-compose -f /home/ubuntu/docker-chapitre12-environnement-symfony/docker-compose.prod.yml stop" \
+# --post-hook "docker-compose -f /home/ubuntu/docker-chapitre12-environnement-symfony/docker-compose.prod.yml start"
